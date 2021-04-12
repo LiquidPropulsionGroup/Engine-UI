0fbcf6e2ea3f591c5a01f75ac8220485c66da367
michael leos <mleos@csus.edu>
2021-04-11 22:57:45 -0700

    Add project files.

---
+++ LPG/BaseControl.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace LPG
+{
+    partial class BaseControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sensorLbl = new System.Windows.Forms.Label();
+            this.currentLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sensorLbl
+            // 
+            this.sensorLbl.AutoSize = true;
+            this.sensorLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sensorLbl.Location = new System.Drawing.Point(3, 8);
+            this.sensorLbl.Name = "sensorLbl";
+            this.sensorLbl.Size = new System.Drawing.Size(120, 24);
+            this.sensorLbl.TabIndex = 0;
+            this.sensorLbl.Text = "sensor name";
+            // 
+            // currentLbl
+            // 
+            this.currentLbl.AutoSize = true;
+            this.currentLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentLbl.Location = new System.Drawing.Point(318, 8);
+            this.currentLbl.Name = "currentLbl";
+            this.currentLbl.Size = new System.Drawing.Size(92, 24);
+            this.currentLbl.TabIndex = 1;
+            this.currentLbl.Text = "cur: value";
+            // 
+            // BaseControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.currentLbl);
+            this.Controls.Add(this.sensorLbl);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "BaseControl";
+            this.Size = new System.Drawing.Size(413, 40);
+            this.Load += new System.EventHandler(this.BaseControl_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BaseControl_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sensorLbl;
+        private System.Windows.Forms.Label currentLbl;
+    }
+}
---
+++ LPG/MainScreen.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace LPG
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.controlBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.DimGray;
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // controlBTN
+            // 
+            this.controlBTN.BackColor = System.Drawing.Color.Firebrick;
+            this.controlBTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.controlBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.controlBTN.Location = new System.Drawing.Point(424, 632);
+            this.controlBTN.Name = "controlBTN";
+            this.controlBTN.Size = new System.Drawing.Size(413, 48);
+            this.controlBTN.TabIndex = 3;
+            this.controlBTN.Text = "FIRE";
+            this.controlBTN.UseVisualStyleBackColor = false;
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.controlBTN);
+            this.Controls.Add(this.menuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LPG-Control";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.Button controlBTN;
+    }
+}
+
